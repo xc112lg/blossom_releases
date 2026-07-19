d03b27ca2bc2fe6feb2b6c8e1221bcbc4c6ed76f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-19 13:22:31 +0800

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -1,1 +1,1 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh) crdroid
+. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh) axion

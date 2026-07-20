2102c6c41c56da04632f6ad1df9ee75689b9452e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-20 13:30:09 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+....#!/bin/bash
 # ==============================================================================
 # blossom superscript — unified build + release dispatcher for Xiaomi "blossom"
 #
@@ -212,7 +212,7 @@ run_lunaris() {
     git clone https://$GH_TOKEN@github.com/xc112lg/blossom_manifest.git -b main .repo/local_manifests
     repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
     /opt/crave/resync.sh
-    rm -rf hardware/lineage/interfaces/sensors
+
     source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh) >/dev/null 2>&1
     . build/envsetup.sh
     export WITH_GMS=false
@@ -238,7 +238,7 @@ run_axion() {
     git clone https://$GH_TOKEN@github.com/xc112lg/blossom_manifest.git -b main .repo/local_manifests
     repo sync -c -j32 --force-sync --no-clone-bundle --no-tags
     /opt/crave/resync.sh
-    rm -rf hardware/lineage/interfaces/sensors
+
     source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh) >/dev/null 2>&1
     . build/envsetup.sh
 

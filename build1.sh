8df4b2e117c0a78f3c87d6dec3f7f77dad0739cb
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-07-24 15:26:43 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -79,7 +79,7 @@ load_env() {
 
 common_prep() {
     load_env
-    #git config --global url."https://${GH_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
+   # git config --global url."https://${GH_TOKEN}:x-oauth-basic@github.com/".insteadOf "https://github.com/"
     git config --global url."https://${GH_TOKEN}@github.com/".insteadOf "https://github.com/"
     rm -rf .repo/local_manifests/
     rm -rf device/xiaomi

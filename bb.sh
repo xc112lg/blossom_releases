3141c30b76a0a10cbf683804a61d91b70dffe7a2
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-14 06:03:42 +0800

    s

--- bb.sh
+++ bb.sh
@@ -1,1 +1,1 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh) evolution
+. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh) alphadroid

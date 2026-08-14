f297588685cd571559d51a6a5c7bb785c8ef4a1e
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-14 14:24:56 +0800

    Update bb.sh

--- bb.sh
+++ bb.sh
@@ -1,1 +1,3 @@
-. <(curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh) alphadroid
+curl -fsSL https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh | bash  -s alphadroid
+
+#curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/bb.sh | bash

d40cd4c3fb1f4462c61c79e8ac38e34ec2505ef4
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-14 14:28:19 +0800

    Update and rename a1.sh to a2.sh

--- a1.sh
+++
@@ -1,1 +0,0 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/bb.sh | bash
---
+++ a2.sh
@@ -0,0 +1,1 @@
+curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/cc.sh | bash

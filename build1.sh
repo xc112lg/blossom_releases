89837807c5477a2894d5d2ed0d482396b753141a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-15 19:10:36 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -88,6 +88,7 @@ common_prep() {
     rm -rf packages/apps/Settings
     rm -rf TMP_PATCHES
     rm -rf hardware/lineage/
+    rm -rf hardware/pixelworks/interfaces
     sudo apt update >/dev/null 2>&1
     sudo apt install patchelf -y >/dev/null 2>&1
 }
@@ -207,7 +208,7 @@ run_alphadroid() {
     sed -i 's|vendor/lineage/|vendor/alpha/|g' device/xiaomi/blossom/lineage_blossom.mk
     grep '^PRODUCT_NAME' device/xiaomi/blossom/lineage_blossom.mk
     mv device/xiaomi/blossom/lineage_blossom.mk device/xiaomi/blossom/alpha_blossom.mk
-
+    git clone https://github.com/AlphaDroid-Project/hardware_pixelworks_interfaces -b alpha-16.2 hardware/pixelworks/interfaces --depth 1
     #source <(curl -sf https://raw.githubusercontent.com/xc112lg/scripts/refs/heads/lunaris/rbe8.sh) >/dev/null 2>&1
     . build/envsetup.sh
     export WITH_GMS=false

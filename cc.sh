ae0b655004548feb7f3ae37e39141b4691b3068b
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-08-20 12:17:43 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,3 +1,3 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh | bash  -s alphadroid  2>&1 | tee build1.log && curl -F "file=@build1.log" https://temp.sh/upload
+curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh | bash  -s lunaris
 
 #curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/bb.sh | bash

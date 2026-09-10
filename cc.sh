8f0110b6161a702e7631af32112d7e1890788712
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-10 15:16:25 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,3 +1,3 @@
-curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh | bash  -s alphadroid
+curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/build1.sh | bash  -s mist
 
 #curl -sf https://raw.githubusercontent.com/xc112lg/blossom_releases/refs/heads/main/bb.sh | bash

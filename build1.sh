4d84add4a701fdb4bd0716a67b2bd1c0f104addf
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-15 18:33:56 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -105,7 +105,7 @@ common_env_exports() {
     export AXION_MAINTAINER=xc112lg
     export ALPHA_MAINTAINER="xc112lg | How's Your Day"
     export MISTOS_MAINTAINER="xc112lg"
-    export INFINITY_MAINTAINER= "xc112lg" 
+    export INFINITY_MAINTAINER=xc112lg
     sed -i '$a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/blossom/lineage_blossom.mk
 }
 
